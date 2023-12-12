29148f5e7db9fb9e14272651f123a7c4a524ad45
Grant Byrne <gbyrne@teraswitch.com>
2023-12-12 12:17:16 -0600

    Initial cut of all the things

---
+++ InstallApps.ps1
@@ -0,0 +1,32 @@
+# Desktop
+# winget install -e --id Google.Chrome
+winget install -e --id Mozilla.Firefox
+winget install -e --id ShareX.ShareX
+winget install -e --id Obsidian.Obsidian
+winget install -e --id Fortinet.FortiClientVPN
+# winget install -e --id AgileBits.1Password
+winget install -e --id 7zip.7zip
+winget install -e --id AutoHotkey.AutoHotkey
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id Postman.Postman
+winget install -e --id Insomnia.Insomnia
+winget install -e --id LINQPad.LINQPad.7
+winget install -e --id dotPDNLLC.paintdotnet
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id SlackTechnologies.Slack
+winget install -e --id Zoom.Zoom
+winget install Microsoft.PowerToys --source winget
+winget install -e --id DebaucheeOpenSourceGroup.Barrier
+
+# Visual Studio
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.VisualStudio.2022.Community.Preview
+
+# Jetbrains
+winget install -e --id JetBrains.Toolbox
+# winget install -e --id JetBrains.Rider
+# winget install -e --id JetBrains.Rider.EAP
+# winget install -e --id JetBrains.DataGrip
+
+# Gaming
+# winget install -e --id Valve.Steam
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Computer-Setup
-Setup Script to Setup My Computer
+
+## The process
+
+### During Installation
+- Set up a partition for development
+- Set up a US keyboard layout
+- Set up the Dvorak keyboard layout
+
+### After OS Installation
+
+- Install Drivers from Computer Manufacter's Website
+- Set up WSL (Windows Subsystem for Linux)
+- Set up a developer partition (64GB)
+    - Create a path for code
+    - Create a path for nuget packages
+- Enable remote script execution
+- Install apps
+- Disable app suggestions
+- Addons for Chrome
+    - Add Ublock Origin
+    - Add Reddit Enhancement Suite
+    - Add in 1Password
+- Addons for Firefox
+    - Add Ublock Origin
+    - Add Reddit Enhancement Suite
+    - Add in 1Password
+- Install Brother Scanner Drivers - https://www.brother-usa.com/support/ds740d
+- Configure Rider
+    - Install CoPilot
+    - Set up Entity Framework Addon
+    - Disable the In-Lay Hints
+    - Map nuget path to developer drive
+- Set up Bluetooth Earbuds
+- Add in file extensions
